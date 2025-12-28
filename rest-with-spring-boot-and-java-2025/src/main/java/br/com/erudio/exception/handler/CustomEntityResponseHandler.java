3f64d19fb98c315ef6fc66f23cd4d140b3b27e9c
Uilham de Oliveira <uilhamgoncalves@gmail.com>
2025-12-28 11:11:03 -0300

    Cria migrations usando Flyway.

--- rest-with-spring-boot-and-java-2025/src/main/java/br/com/erudio/exception/handler/CustomEntityResponseHandler.java
+++ rest-with-spring-boot-and-java-2025/src/main/java/br/com/erudio/exception/handler/CustomEntityResponseHandler.java
@@ -1,7 +1,7 @@
 package br.com.erudio.exception.handler;
 
 import br.com.erudio.exception.ExceptionResponse;
-import br.com.erudio.exception.UnsupportedMathOperationException;
+import br.com.erudio.exception.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -27,13 +27,13 @@ public class CustomEntityResponseHandler extends ResponseEntityExceptionHandler 
     }
 
 
-    @ExceptionHandler(UnsupportedMathOperationException.class)
-    public final ResponseEntity<ExceptionResponse> handleBadRequestExceptions(Exception ex, WebRequest request){
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public final ResponseEntity<ExceptionResponse> handleNotFoudExceptions(Exception ex, WebRequest request){
         ExceptionResponse response = new ExceptionResponse(
                 new Date(),
                 ex.getMessage(),
                 request.getDescription(false)
         );
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 }
