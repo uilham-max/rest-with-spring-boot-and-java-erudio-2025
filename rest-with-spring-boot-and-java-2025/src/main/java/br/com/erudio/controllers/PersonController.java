b88aaab3a9e81e447784fe7bcc6b86a919eeb4d1
Uilham de Oliveira <uilhamgoncalves@gmail.com>
2026-06-04 20:39:16 -0300

    Implementa documentação Swagger e Springdoc em Person.

--- rest-with-spring-boot-and-java-2025/src/main/java/br/com/erudio/controllers/PersonController.java
+++ rest-with-spring-boot-and-java-2025/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -1,18 +1,19 @@
 package br.com.erudio.controllers;
 
+import br.com.erudio.controllers.docs.PersonControllerDocs;
 import br.com.erudio.data.dto.v1.PersonDTO;
-import br.com.erudio.data.dto.v2.PersonDTOV2;
 import br.com.erudio.services.impl.PersonServiceImpl;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/person/v1")
-public class PersonController {
+@Tag(name="People", description = "Endpoints for Managing People")
+public class PersonController implements PersonControllerDocs {
 
     private PersonServiceImpl personServices;
 
@@ -21,8 +22,9 @@ public class PersonController {
     }
 
     @PostMapping(
-            consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE},
-            produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE })
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE})
+    @Override
     public ResponseEntity<PersonDTO> create(@RequestBody PersonDTO personDTO) {
         PersonDTO entity = personServices.create(personDTO);
         return ResponseEntity.ok(entity);
@@ -30,32 +32,32 @@ public class PersonController {
 
 
     @GetMapping(
-            produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE })
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE})
+    @Override
     public List<PersonDTO> findAll(){
         return personServices.findAll();
     }
 
     @GetMapping(value = "/{id}",
-            produces =  { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE })
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE})
+    @Override
     public PersonDTO findById(@PathVariable("id") Long id) {
-        PersonDTO personDTO = personServices.findById(id);
-        return personDTO;
+        return personServices.findById(id);
     }
 
     @PutMapping(
-            consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE },
-            produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE })
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE})
+    @Override
     public PersonDTO update(@RequestBody PersonDTO personDTO) {
         return personServices.update(personDTO);
     }
 
     @DeleteMapping(value = "/{id}")
+    @Override
     public ResponseEntity<?> delete(@PathVariable Long id){
         personServices.delete(id);
         return ResponseEntity.noContent().build();
     }
-
-
-
 
 }
