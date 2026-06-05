86dfcf7c8ccbd5c9f1824aaa6fa91103ab00d90f
Uilham de Oliveira <uilhamgoncalves@gmail.com>
2026-06-04 22:20:49 -0300

    Implementa documentação Swagger e Springdoc em Person.

--- rest-with-spring-boot-and-java-2025/src/main/java/br/com/erudio/mapper/ObjectMapper.java
+++ rest-with-spring-boot-and-java-2025/src/main/java/br/com/erudio/mapper/ObjectMapper.java
@@ -6,27 +6,10 @@ import com.github.dozermapper.core.Mapper;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Classe utilitária responsável por realizar o mapeamento
- * entre objetos utilizando a biblioteca Dozer.
- *
- * O objetivo desta classe é facilitar a conversão entre:
- *
- * - Entidades (Entity)
- * - DTOs (Data Transfer Object)
- * - View Objects
- *
- * Exemplo:
- *
- * PersonEntity -> PersonDTO
- * PersonDTO -> PersonEntity
- *
- * O Dozer faz o mapeamento automaticamente entre atributos
- * com nomes compatíveis.
- */
 public class ObjectMapper {
 
     private static Mapper mapper = DozerBeanMapperBuilder.buildDefault();
+
     public static <O, D> D parseObject(O origin, Class<D> destination) {
         return mapper.map(origin, destination);
     }
