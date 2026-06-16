cd45c731789ae932405308d84c70a0a25c48649c
Uilham de Oliveira <uilhamgoncalves@gmail.com>
2026-06-15 21:11:41 -0300

    Implementa CORS.

--- rest-with-spring-boot-and-java-2025/src/main/java/br/com/erudio/config/WebConfig.java
+++ rest-with-spring-boot-and-java-2025/src/main/java/br/com/erudio/config/WebConfig.java
@@ -1,30 +1,30 @@
 package br.com.erudio.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${cors.originPatterns}")
+    private String corsOriginPatterns = "";
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        var allowedOrigins = corsOriginPatterns.split(",");
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins(allowedOrigins)
+                .allowCredentials(true);
+    }
+
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
 
-        // Via EXTENSION http://localhost:8080/api/person/v1/1.xml or http://localhost:8080/api/person/v1/1.json
-
-        // Via QUERY PARAM http://localhost:8080/api/person/v1/1?mediaType=xml
-        /**
-        configurer.favorParameter(true)
-                .parameterName("mediaType")
-                .ignoreAcceptHeader(true)
-                .useRegisteredExtensionsOnly(false)
-                .defaultContentType(MediaType.APPLICATION_JSON)
-                .mediaType("json", MediaType.APPLICATION_JSON)
-                .mediaType("xml", MediaType.APPLICATION_XML);
-        */
-
-        // Via HEADER PARAM http://localhost:8080/api/person/v1/1
         configurer.favorParameter(false)
                 .ignoreAcceptHeader(false)
                 .useRegisteredExtensionsOnly(false)
--- rest-with-spring-boot-and-java-2025/src/main/java/br/com/erudio/controllers/PersonController.java
+++ rest-with-spring-boot-and-java-2025/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+//@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping("/api/person/v1")
 @Tag(name="People", description = "Endpoints for Managing People")
@@ -21,6 +22,8 @@ public class PersonController implements PersonControllerDocs {
         this.personServices = personServices;
     }
 
+
+//    @CrossOrigin(origins = {"http://localhost:8080", "https://reboquesoliveira.com"})
     @PostMapping(
             consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE},
             produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE})
@@ -38,12 +41,15 @@ public class PersonController implements PersonControllerDocs {
         return personServices.findAll();
     }
 
+
+//    @CrossOrigin(origins = "http://localhost:8080")
     @GetMapping(value = "/{id}",
             produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE})
     @Override
     public PersonDTO findById(@PathVariable("id") Long id) {
         return personServices.findById(id);
     }
+
 
     @PutMapping(
             consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE},
@@ -52,6 +58,7 @@ public class PersonController implements PersonControllerDocs {
     public PersonDTO update(@RequestBody PersonDTO personDTO) {
         return personServices.update(personDTO);
     }
+
 
     @DeleteMapping(value = "/{id}")
     @Override
